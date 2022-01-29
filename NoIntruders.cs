df7ee1d5f694d110e47205f1fe82e296069dfe1b
Zemogiter <zumi999@vp.pl>
2022-01-29 15:53:18 +0100

    Add files via upload

---
+++ NoIntruders.cs
@@ -0,0 +1,56 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityModManagerNet;
+
+//getHeight, getIcon, getSelectionBounds and getAnimationNames are placeholders to allow the code to compile, do not touch them unless needed
+namespace NoIntruders
+{
+    public class NoIntruders : Character
+    {
+        public override float getHeight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Texture2D getIcon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Bounds getSelectionBounds()
+        {
+            throw new NotImplementedException();
+        }
+        [LoaderOptimization(LoaderOptimization.NotSpecified)]
+        public static void Init(UnityModManager.ModEntry modData)
+        {
+            modData.OnUpdate = Update;
+        }
+
+        public static void Update(UnityModManager.ModEntry modData, float timeStep)
+        {
+            NoIntruders kill = new NoIntruders();            
+            List<Character> intruders = getSpecializationCharacters(SpecializationList.IntruderInstance);
+            if (intruders != null)
+            {
+                foreach (Character intruder in intruders)
+                {
+                    // kill intruders instantly.
+                    if (intruder != null && !intruder.isDead())
+                    {
+                        intruder.setArmed(false);
+                        kill.setDead();
+
+                    }
+                }
+            }
+        }
+
+        protected override List<string> getAnimationNames(CharacterAnimationType animationType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
